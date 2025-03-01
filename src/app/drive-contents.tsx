6852cd08f8a090b8b8f4ba94c35f092d48c05df4
Jasdeep Singh <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2025-03-01 02:08:27 -0500

    Implement breadcrumbs

--- src/app/drive-contents.tsx
+++ src/app/drive-contents.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 export default function DriveContents(props: {
   files: (typeof files.$inferSelect)[];
   folders: (typeof folders.$inferSelect)[];
+  parents: (typeof folders.$inferSelect)[];
 }) {
   const breadcrumbs: unknown[] = [];
 
@@ -28,7 +29,7 @@ export default function DriveContents(props: {
             >
               My Drive
             </Link>
-            {breadcrumbs.map((folder) => (
+            {props.parents.map((folder) => (
               <div key={folder.id} className="flex items-center">
                 <ChevronRight className="mx-2 text-gray-500" size={16} />
                 <Link
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-import { db } from "~/server/db";
-import {
-  files as FileSchema,
-  folders as FolderSchema,
-} from "~/server/db/schema";
-import DriveContents from "./drive-contents";
-import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
 
 export default async function GoogleDriveClone() {
-  const files = await db
-    .select()
-    .from(FileSchema)
-    .where(eq(FileSchema.parent, 1));
-  const folders = await db
-    .select()
-    .from(FolderSchema)
-    .where(eq(FolderSchema.parent, 1));
-  return <DriveContents files={files} folders={folders} />;
+  redirect("/f/1");
 }
